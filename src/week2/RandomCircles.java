c1cdfcfd6641a2ce4437642007afa368979952b5
yourUsername <yourEmail@domain.com>
2021-02-19 00:16:04 -0500

    ff

--- src/week2/RandomCircles.java
+++ src/week2/RandomCircles.java
@@ -1,7 +1,22 @@
 package week2;
 
+import acm.graphics.GOval;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class RandomCircles extends GraphicsProgram {
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
 
 }
